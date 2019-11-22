d28cd2413c48c73a6457cf7107d6cdfd0c611d8b
dsreliete <dsreliete@gmail.com>
2019-11-22 04:24:24 -0500

    new change3

--- airline/settings/production.py
+++ airline/settings/production.py
@@ -2,7 +2,7 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['django-airlines.heroku.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.django-airlines.heroku.com']
 
 DB_URL = get_env_variable('DATABASE_URL')
 db_from_env = dj_database_url.config(DB_URL)
