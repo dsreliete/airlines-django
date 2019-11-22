f3dd12880d459844894ef1fc693d4cfcb30eb185
dsreliete <dsreliete@gmail.com>
2019-11-22 13:51:38 -0500

    idontgiveup

---
+++ secrets.py
@@ -0,0 +1,7 @@
+export SECRET_KEY='$*$rx0^hnq5l-nfpp^8i$qy#u$5@g^*0sd#hlcuqy$^5)qk)un'
+export DB_NAME="travel"
+export DB_USER="elieterodrigues"
+export DB_PSWD='55851296'
+
+admin postgres dsreliete
+admin sqlite elieterodrigues
