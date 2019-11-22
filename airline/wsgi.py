8e98f7f8c9ef5df65b4a87712decd862d5ba8960
dsreliete <dsreliete@gmail.com>
2019-11-22 11:03:21 -0500

    new day new try

--- airline/wsgi.py
+++ airline/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ['DJANGO_SETTINGS_MODULE'])
 
 application = get_wsgi_application()
+
